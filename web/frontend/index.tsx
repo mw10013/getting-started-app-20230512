11ffb6f7ab7b2656deab2002c72cf94a6fac6e17
Michael Wu <mw10013@gmail.com>
2023-05-03 10:38:15 -0400

    .

--- web/frontend/index.tsx
+++ web/frontend/index.tsx
@@ -1,5 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.tsx'
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
