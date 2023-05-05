aa9d75bc311fc49ce1a95b66a363209238fd7a1b
Michael Wu <mw10013@gmail.com>
2023-05-04 22:09:47 -0400

    .

--- web/frontend/components/providers/PolarisProvider.tsx
+++ web/frontend/components/providers/PolarisProvider.tsx
@@ -1,17 +1,27 @@
-import { useCallback } from "react";
+import React, { useCallback } from "react";
 import { AppProvider } from "@shopify/polaris";
 import { useNavigate } from "@shopify/app-bridge-react";
 import translations from "@shopify/polaris/locales/en.json";
 import "@shopify/polaris/build/esm/styles.css";
 import type { PropsWithChildren } from "react";
+import { LinkLikeComponentProps } from "@shopify/polaris/build/ts/latest/src/utilities/link";
+// import { LinkLikeComponent } from "@shopify/polaris/build/ts/latest/src/utilities/link/types";
 
+type AppBridgeLinkProps = Parameters<
+  Extract<
+    ConstructorParameters<typeof AppProvider>[0]["linkComponent"],
+    React.FunctionComponent<any>
+  >
+>[0];
 
 function AppBridgeLink({
   url,
   children,
   external,
   ...rest
-}: PropsWithChildren<{ url: string; external: boolean }>) {
+}: 
+// }: LinkLikeComponentProps) {
+AppBridgeLinkProps) {
   const navigate = useNavigate();
   const handleClick = useCallback(() => {
     navigate(url);
