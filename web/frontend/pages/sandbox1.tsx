d143bb186c851f9f2ff4a32556fac99394f7a514
Michael Wu <mw10013@gmail.com>
2023-05-10 21:08:56 -0400

    .

--- web/frontend/pages/sandbox1.tsx
+++ web/frontend/pages/sandbox1.tsx
@@ -1,11 +1,11 @@
-import { Card, Page, Layout, TextContainer, Heading } from "@shopify/polaris";
+import { Page } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
 
 export default function Sandbox() {
   return (
     <Page>
       <TitleBar
-        title="Sandbox1"
+        title="Sandbox1!!"
         breadcrumbs={[{ content: "Home", url: "/" }]}
         primaryAction={{
           content: "Primary action",
@@ -19,30 +19,6 @@ export default function Sandbox() {
           },
         ]}
       />
-      <Layout>
-        <Layout.Section>
-          <Card sectioned>
-            <Heading>Heading</Heading>
-            <TextContainer>
-              <p>Body</p>
-            </TextContainer>
-          </Card>
-          <Card sectioned>
-            <Heading>Heading</Heading>
-            <TextContainer>
-              <p>Body</p>
-            </TextContainer>
-          </Card>
-        </Layout.Section>
-        <Layout.Section secondary>
-          <Card sectioned>
-            <Heading>Heading</Heading>
-            <TextContainer>
-              <p>Body</p>
-            </TextContainer>
-          </Card>
-        </Layout.Section>
-      </Layout>
     </Page>
   );
 }
